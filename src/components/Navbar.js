dee4316668f2d89a6e9ef258cd4ea4731627dddf
ismail1011 <imshaikh1011@gmail.com>
2023-03-26 16:28:23 +0530

    New Project Creation

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import { click } from '@testing-library/user-event/dist/click'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+
+   const [click, setClick] = useState(false);
+   const handleClick = () => setClick(!click);
+   const closeMobileMenu = () => setClick(false);
+
+   return (
+      <>
+         <nav className='navbar'>
+            <div className='navbar-container'>
+               <img className='logo' src='/public/Images/sk logo.png' />
+            </div>
+
+            <div className='menu-icon' onClick={handleClick}>
+               <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+
+            <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+               <li className='nav-item'>
+                  <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                     Home
+                  </Link>
+               </li>
+
+               <li className='nav-item'>
+                  <Link to='/Services' className='nav-links' onClick={closeMobileMenu}>
+                     Services
+                  </Link>
+               </li>
+            </ul>
+
+         </nav>
+      </>
+   )
+}
+
+export default Navbar
